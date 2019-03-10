7f290397f651a35be4f32d56eb593aa20f52fa88
Fabrice Bauzac <fbauzac@amadeus.com>
2019-03-10 10:01:21 +0100

    First working example

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# JavaEE REST boilerplate (work in progress)
+# JavaEE REST boilerplate
 
-To build:
+## Building
 
 ``` shell
 $ mvn package
 ```
+
+## Deploying
+
+Boot a Wildfly AS.
+
+Deploy:
+
+``` shell
+$ mvn wildfly:deploy
+```
+
+Enjoy: http://127.0.0.1:8080/boil/resources/helloworld
--- src/main/java/fr/fbauzac/boil/HelloWorld.java
+++ src/main/java/fr/fbauzac/boil/HelloWorld.java
@@ -8,26 +8,27 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ApplicationPath;
- 
+
 /**
  * Root resource (exposed at "helloworld" path)
  */
-@ApplicationPath("helloworld")
+@Path("helloworld")
+@Produces("application/json")
+@Consumes("application/json")
 public class HelloWorld {
     @Context
     private UriInfo context;
- 
+
     /** Creates a new instance of HelloWorld */
     public HelloWorld() {
     }
- 
+
     /**
      * Retrieves representation of an instance of helloWorld.HelloWorld
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces("text/html")
-    public String getHtml() {
-        return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
+    public String httpGet() {
+        return "{\"two\": 2, \"four\": 4}";
     }
 }
